e1761140d00a66ba437b9f5b7c7e1e7e6177885d
polmic <pmicas319@gmail.com>
2025-05-12 11:36:14 +0200

    [guessing_game] read input & rand

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use rand::Rng;
+
+fn generate_secret() -> i32 {
+    rand::rng().random_range(1..=100)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let secret = generate_secret();
+
+    println!("{secret}");
+
+    println!("Guess the number:");
+    println!("Input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess)
 }
