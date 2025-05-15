473983026a822e19ee1bd1da9dbeb059c55578f2
polmic <pmicas319@gmail.com>
2025-05-15 16:48:06 +0200

    [struct example] area

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -1,3 +1,10 @@
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
 fn main() {
-    println!("Hello, world!");
+    let width = 30;
+    let height = 30;
+    
+    println!("Area: {}", area(width, height));
 }
