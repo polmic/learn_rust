241016171e55f2472b4b23617b84002bfaa737cf
polmic <pmicas319@gmail.com>
2025-05-15 22:06:22 +0200

    [minigrep] grep logic & test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -18,10 +18,41 @@ impl Config {
     }
 }
 
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("searching {} in {}", config.query, config.filepath);
     let contents = fs::read_to_string(config.filepath).expect("could not read file");
-    println!("{}", contents);
 
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+    
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
 }
