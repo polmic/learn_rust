09515cff89a6c8490afaa9af5a9d6ca3a1f59f63
polmic <pmicas319@gmail.com>
2025-05-15 22:16:48 +0200

    [minigrep] stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error parsing arguments: {err}");
+        eprintln!("Error parsing arguments: {err}");
         process::exit(1)
     });
 
     if let Err(e) = run(config) {
-        println!("Application Error: {e}");
+        eprintln!("Application Error: {e}");
         process::exit(1);
     }
 }
