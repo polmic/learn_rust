4330fe297f9fd6a97edc4f20560232c2a178f2d3
polmic <pmicas319@gmail.com>
2025-05-15 18:54:13 +0200

    [struct example] tests

--- struct_example/src/rectangle.rs
+++ struct_example/src/rectangle.rs
@@ -22,3 +22,43 @@ impl Rectangle {
     }
 
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_square() {
+        let rect = Rectangle::square(10);
+        assert_eq!(rect.width, 10);
+        assert_eq!(rect.height, 10);
+    }
+
+    #[test]
+    fn test_area() {
+        let rect = Rectangle::new(20, 10);
+        assert_eq!(rect.area(), 200);
+    }
+
+    #[test]
+    fn test_can_hold_true() {
+        let rect1 = Rectangle::new(20, 10);
+        let rect2 = Rectangle::new(10, 5);
+        assert_eq!(rect1.can_hold(&rect2), true);
+    }
+
+    #[test]
+    fn test_can_hold_false_inferior_width() {
+        let rect1 = Rectangle::new(20, 10);
+        let rect2 = Rectangle::new(21, 2);
+        assert_eq!(rect1.can_hold(&rect2), false);
+    }
+
+    #[test]
+    fn test_can_hold_false_inferior_height() {
+        let rect1 = Rectangle::new(20, 10);
+        let rect2 = Rectangle::new(3, 11);
+        assert_eq!(rect1.can_hold(&rect2), false);
+    }
+    
+}
