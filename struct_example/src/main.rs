f8039be1fd972a52debcf33c1cd3bbb716b6f292
polmic <pmicas319@gmail.com>
2025-05-15 17:21:42 +0200

    [struct example] can_hold && square

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -3,9 +3,18 @@ mod rectangle;
 use rectangle::Rectangle;
 
 fn main() {
-    let rectangle = Rectangle::new(32, 32);
+    let r1 = Rectangle::new(10, 10);
+    let r2 = Rectangle::new(100, 100);
 
-    println!("{:?}", rectangle);
+    println!("r1: {:?} - r2: {:?}", r1, r2);
+    println!("r1.area: {} - r2.area: {}", r1.area(), r2.area());
+    println!("can r1 hold r2: {}", r1.can_hold(&r2));
+    println!("can r2 hold r1: {}", r2.can_hold(&r1));
 
-    println!("Area: {}", rectangle.area());
+    println!();
+
+    let s1 = Rectangle::square(100);
+    let s2 = Rectangle::square(100);
+    println!("s1: {:?} - s2: {:?}", s1, s2);
+    println!("can s1 hold s2: {}", s1.can_hold(&s2));
 }
--- struct_example/src/rectangle.rs
+++ struct_example/src/rectangle.rs
@@ -8,8 +8,17 @@ impl Rectangle {
     pub fn new(width: u32, height: u32) -> Self {
         Rectangle { width, height }
     }
+    
+    pub fn square(size: u32) -> Self {
+        Self::new(size, size)
+    }
 
     pub fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    pub fn can_hold(&self, r2: &Rectangle) -> bool {
+        &self.width > &r2.width && &self.height > &r2.height
+    }
+
 }
