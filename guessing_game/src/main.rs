9f96c2ba2f7ade3c93d1dfa7f2026bf122b04bb2
polmic <pmicas319@gmail.com>
2025-05-15 13:02:48 +0200

    [guessing_game] compare_secret_to_guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,17 @@
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
-fn generate_secret() -> i32 {
+fn generate_secret() -> u32 {
     rand::rng().random_range(1..=100)
+}
+
+fn compare_secret_to_guess(secret: u32, guess: u32) {
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
 
 fn main() {
@@ -13,11 +22,15 @@ fn main() {
     println!("Guess the number:");
     println!("Input your guess");
 
-    let mut guess = String::new();
+    let mut guess: String = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess)
+    println!("You guessed: {}", guess);
+    
+    let guess: u32 = guess.trim().parse().expect("Not a valid positive number");
+    
+    compare_secret_to_guess(secret, guess);
 }
